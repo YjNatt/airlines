1d1807dc9ee332db3b13e6ad044a5955b8bfa7a2
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-08-20 11:00:52 -0700

    add select to filter routes by airline

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import airlineData, { getAirlineById, getAirportByCode } from './data';
 import Table from './components/Table';
@@ -22,16 +22,42 @@ const App = () => {
     return fetchedValue.name;
   }
 
+  const [airlineId, setAirlineId] = useState(0);
+
+  const airlineSelectHandle = (event) => {
+    setAirlineId(Number(event.target.value))
+  }
+
+  const filterRoutes = () => {
+    if (airlineId === 0) {
+      return airlineData.routes;
+    }
+
+    return airlineData.routes.filter(routes => {
+      return routes.airline === airlineId
+    })
+  }
+
   return (
     <div className="app">
     <header className="header">
       <h1 className="title">Airline Routes</h1>
     </header>
     <section>
+      <div>
+        <select name="airline" id="airline-select" onChange={airlineSelectHandle}>
+          <option value="0">All Airlines</option>
+          {airlineData.airlines.map(airline => {
+            return (
+              <option key={airline.id} value={airline.id}>{airline.name}</option>
+            )
+          })}
+        </select>
+      </div>
       <Table
         className="routes-table"
         columns={columns}
-        rows={airlineData.routes}
+        rows={filterRoutes()}
         format={formatValue}
         perPage={25}
       />
