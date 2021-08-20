cdec72d2a0db71a288cbd3455ad7e1bdcb59a7e9
Yujohn Nattrass <yujohn.natt@gmail.com>
2021-08-20 10:10:11 -0700

    display message of the current number of routes showing

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react'
 
 const Table = ({ columns, rows, format }) => {
-  let currentRows = rows.slice(0, 25)
+  let currentIndex = 0;
+  let currentRows = rows.slice(currentIndex, currentIndex + 25)
 
   return(
-    <table>
-      <thead>
-        <tr>
-          {columns.map(col => {
-            return <th key={col.property}>{col.name}</th>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {columns.map(col => {
+              return <th key={col.property}>{col.name}</th>
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {currentRows.map((row, index) => {
+            return(
+              <tr key={index}>
+                <td>{format('airline', row)}</td>
+                <td>{format('src', row)}</td>
+                <td>{format('dest', row)}</td>
+              </tr>
+            )
           })}
-        </tr>
-      </thead>
-      <tbody>
-        {currentRows.map((row, index) => {
-          return(
-            <tr key={index}>
-              <td>{format('airline', row)}</td>
-              <td>{format('src', row)}</td>
-              <td>{format('dest', row)}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+      <div>
+        <p>Showing {currentIndex + 1} - {currentIndex + 25} of {rows.length} total routes</p>
+      </div>
+    </div>
   )
 }
 
